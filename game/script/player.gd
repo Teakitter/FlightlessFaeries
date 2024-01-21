02e068ed2be904927cddb23cf6b9084e4537772f
cats <143235975+fluffyfelidae@users.noreply.github.com>
2024-01-21 17:00:43 +0800

    player

---
+++ game/script/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var SPEED = 400
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
+	
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
