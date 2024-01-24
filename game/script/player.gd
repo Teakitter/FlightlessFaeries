15e0cba8344ba5879ad4626967ee0de58f417d05
cats <143235975+fluffyfelidae@users.noreply.github.com>
2024-01-24 17:25:11 +0800

    running sound only starts AFTER player starts game

--- game/script/player.gd
+++ game/script/player.gd
@@ -3,9 +3,8 @@ extends CharacterBody2D
 const GRAVITY : int = 150
 const JUMP_SPEED : int = -1800
 
-# Called every frame.
-
 func _physics_process(delta):
+
 	velocity.y += GRAVITY + delta
 	if is_on_floor():
 		if Input.is_action_pressed("jump"):
@@ -15,7 +14,8 @@ func _physics_process(delta):
 		#elif Input.is_action_pressed("duck"):
 			#$AnimatedSprite2D.play("duck")
 	else:
-		$SoundRun.play()
+		if Input.is_action_pressed("jump"):
+			$SoundRun.play()
 			#$AnimatedSprite2D.play("run")
 	#else:
 		#$animatedSprite2D.play("jump")
