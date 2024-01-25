1993232f22e048496a656a18293016d7e61438a6
cats <143235975+fluffyfelidae@users.noreply.github.com>
2024-01-25 14:28:16 +0800

    messed with the jump audio some more

--- game/script/player.gd
+++ game/script/player.gd
@@ -7,17 +7,23 @@ func _physics_process(delta):
 	velocity.y += GRAVITY + delta
 	
 	if is_on_floor():
-		if Input.is_action_pressed("jump"):
-			$SoundJump.play()
-			velocity.y = JUMP_SPEED
-		
-		#elif Input.is_action_pressed("duck"):
-			#$AnimatedSprite2D.play("duck")
-	else:
-		if Input.is_action_pressed("jump"):
-			$SoundRun.play()
-			#$AnimatedSprite2D.play("run")
+		if not get_parent().game_running:
+			pass
+		else:
+			if Input.is_action_pressed("jump"):
+				$SoundRun.stop()
+				velocity.y = JUMP_SPEED
+				$SoundJump.play()
+				if is_on_floor():
+					$SoundRun.play()
+			
+			#elif Input.is_action_pressed("duck"):
+				#$AnimatedSprite2D.play("duck")
+			else:
+				pass
+				#$AnimatedSprite2D.play("run")
 	#else:
 		#$animatedSprite2D.play("jump")
 	
 	move_and_slide()
+
